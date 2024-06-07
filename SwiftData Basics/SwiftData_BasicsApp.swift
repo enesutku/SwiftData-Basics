cfb2226f4895901220b77d63fabd18454aa93dc4
Enes UTKU <ensutku@hotmail.com>
2024-06-08 01:20:54 +0300

    Initial Commit

---
+++ SwiftData Basics/SwiftData_BasicsApp.swift
@@ -0,0 +1,12 @@
+// Created by Enes UTKU
+
+import SwiftUI
+
+@main
+struct SwiftData_BasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
